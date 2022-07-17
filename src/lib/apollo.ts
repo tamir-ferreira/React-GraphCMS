da8e7ba590006ddf0c00cc3d043606f6b6dddc88
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2022-07-17 20:34:09 -0300

    encoding data with .env file

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,6 +2,15 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 /* linkando com a API do graphCMS */
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4r600ju0ot301yw6ayo55k5/master',
+    /* em javaScript utilizamos process.env para importar variáveis de ambiente */
+    /* aqui utilizamos import.meta.env */
+
+    /* Necessário para acessar o Graph CMS e consultar os dados das aulas */
+    uri: import.meta.env.VITE_API_URL,
+
+    /* Necessário para enviar ao Graph CMS os dados de preenchimento do formulário */
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
          cache: new InMemoryCache()
 })
